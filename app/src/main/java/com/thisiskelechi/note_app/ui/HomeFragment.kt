c7bac1597d03665c9908db6961465d7905659914
kelechi <oparaiskelechi@gmail.com>
2020-02-22 02:47:12 +0100

    done with the ui and navigations

---
+++ app/src/main/java/com/thisiskelechi/note_app/ui/HomeFragment.kt
@@ -0,0 +1,35 @@
+package com.thisiskelechi.note_app.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.thisiskelechi.note_app.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_add.setOnClickListener {
+         val action = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
